c12128bba08d72c46b23ebd3f2b5efb5e52bf983
ramroumette <mr.toumirami@outlook.fr>
2024-03-06 03:35:00 +0100

    changes configurations and adding migrations versions

--- config/bundles.php
+++ config/bundles.php
@@ -6,12 +6,7 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-<<<<<<< HEAD
-
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-
-=======
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
->>>>>>> gestion_utilisateur
 ];
