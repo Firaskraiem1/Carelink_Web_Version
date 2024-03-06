901f41527cbbab61371b3d58cdfde70ebcf7bb71
Firaskraiem1 <firasbenkraiem8@gmail.com>
2024-03-06 11:54:54 +0100

    configuration removed

--- src/Controller/FichePatientController.php
+++ src/Controller/FichePatientController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\FichePatient;
+use App\Entity\Patient;
 use App\Form\FichePatientType;
 use App\Repository\FichePatientRepository;
 use App\Repository\PatientRepository;
@@ -16,12 +17,44 @@ use Symfony\Component\Routing\Attribute\Route;
 class FichePatientController extends AbstractController
 {
     #[Route('/', name: 'app_fiche_patient_index', methods: ['GET'])]
-    public function index(FichePatientRepository $fichePatientRepository): Response
+    public function index(): Response
     {
-        return $this->render('fiche_patient/index.html.twig', [
+        return $this->render('fiche_patient/index.html.twig');
+    }
+
+    #[Route('/list', name: 'app_fiche_patient_listes', methods: ['GET'])]
+    public function list(FichePatientRepository $fichePatientRepository): Response
+    {
+        return $this->render('fiche_patient/listes.html.twig', [
+            // 'patientFiche' => 'false',
             'fiche_patients' => $fichePatientRepository->findAll(),
         ]);
     }
+
+    //------------------------Patient--------------------
+
+
+    #[Route('/list/{idPatient}', name: 'app_fiche_list_patient', methods: ['GET'])]
+    public function listFichesPatient(FichePatientRepository $fichePatientRepository, EntityManagerInterface $entityManager, $idPatient): Response
+    {
+        $patient = $entityManager->getRepository(Patient::class)->find($idPatient);
+        $fiche_patients = $fichePatientRepository->findBy(['relationPatient' => $patient]);
+        return $this->render('patient/listes_fiches.html.twig', [
+            'idPatient' => $idPatient,
+            'fiche_patients' => $fiche_patients,
+        ]);
+    }
+    #[Route('/{id}/{idPatient}/show', name: 'app_fiche_show_patient', methods: ['GET'])]
+    public function showFichePatient(FichePatient $fichePatient, $idPatient): Response
+    {
+        return $this->render('patient/showFiche.html.twig', [
+            'id' => $idPatient,
+            'fiche_patient' => $fichePatient,
+        ]);
+    }
+
+    //------------------end------------------------
+
 
     #[Route('/new', name: 'app_fiche_patient_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
@@ -33,8 +66,7 @@ class FichePatientController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($fichePatient);
             $entityManager->flush();
-
-            return $this->redirectToRoute('app_fiche_patient_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_fiche_patient_listes', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('fiche_patient/new.html.twig', [
@@ -51,9 +83,8 @@ class FichePatientController extends AbstractController
         ]);
     }
 
-
-    #[Route('/{id}/edit', name: 'app_fiche_patient_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, FichePatient $fichePatient, EntityManagerInterface $entityManager, int $id, FichePatientRepository $rep): Response
+    #[Route('/edit/{id}', name: 'app_fiche_patient_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, EntityManagerInterface $entityManager, int $id, FichePatientRepository $rep): Response
     {
         $fichePatient = $rep->find($id);
         $form = $this->createForm(FichePatientType::class, $fichePatient);
@@ -61,7 +92,7 @@ class FichePatientController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($fichePatient);
             $entityManager->flush();
-            return $this->redirectToRoute('app_fiche_patient_index');
+            return $this->redirectToRoute('app_fiche_patient_listes');
         }
         return $this->render('fiche_patient/edit.html.twig', [
             'fiche_patient' => $fichePatient,
@@ -77,4 +108,82 @@ class FichePatientController extends AbstractController
         $entityManager->flush();
         return $this->redirectToRoute('app_fiche_patient_index', [], Response::HTTP_SEE_OTHER);
     }
+
+
+
+
+    #------------------------------------ADMIN-------------------------------------------#
+
+    #----------------admin index----------------
+    #[Route('/admin', name: 'app_fiche_patient_listes_admin', methods: ['GET'])]
+    public function indexAdmin(FichePatientRepository $fichePatientRepository): Response
+    {
+        return $this->render('admin/dashboard__tables.html.twig', [
+            'fiche_patients' => $fichePatientRepository->findAll(),
+        ]);
+    }
+    #----------------end----------------
+
+    #----------------admin show fiche----------------
+    #[Route('/admin/{id}', name: 'app_fiche_patient_show_admin', methods: ['GET'])]
+    public function showficheadmin(FichePatient $fichePatient): Response
+    {
+        return $this->render('admin/dashboard__show__fiche.html.twig', [
+            'fiche_patient' => $fichePatient,
+        ]);
+    }
+    #----------------end----------------
+
+    #----------------admin new fiche----------------
+    #[Route('/admin/fiche/new', name: 'app_fiche_patient_new_admin', methods: ['GET', 'POST'])]
+    public function newFicheAdmin(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $fichePatient = new FichePatient();
+        $form = $this->createForm(FichePatientType::class, $fichePatient);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($fichePatient);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_fiche_patient_listes_admin', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/dashboard__new_fiche.html.twig', [
+            'fiche_patient' => $fichePatient,
+            'form' => $form,
+        ]);
+    }
+
+    #----------------end----------------
+
+    #----------------admin edit fiche----------------
+    #[Route('/admin/{id}/edit', name: 'app_fiche_patient_edit_admin', methods: ['GET', 'POST'])]
+    public function editFiche(Request $request, EntityManagerInterface $entityManager, int $id, FichePatientRepository $rep): Response
+    {
+        $fichePatient = $rep->find($id);
+        $form = $this->createForm(FichePatientType::class, $fichePatient);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($fichePatient);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_fiche_patient_listes_admin');
+        }
+        return $this->render('admin/dashboard__update__fiche.html.twig', [
+            'fiche_patient' => $fichePatient,
+            'form' => $form,
+        ]);
+    }
+    #----------------end----------------
+
+    #----------------admin delete fiche ----------------
+    #[Route('/admin/{id}', name: 'app_fiche_patient_delete_admin', methods: ['POST'])]
+    public function deleteFiche(EntityManagerInterface $entityManager, FichePatientRepository $rep, $id): Response
+    {
+        $fichePatient = $rep->find($id);
+        $entityManager->remove($fichePatient);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_fiche_patient_listes_admin', [], Response::HTTP_SEE_OTHER);
+    }
+    #----------------end----------------
 }
--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -3,23 +3,29 @@
 namespace App\Controller;
 
 use App\Entity\Patient;
+use App\Form\JoinEspaceType;
 use App\Form\PatientType;
+use App\Model\JoinEspace;
 use App\Repository\PatientRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use MercurySeries\FlashyBundle\FlashyNotifier;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[Route('/patient')]
 class PatientController extends AbstractController
 {
     #[Route('/', name: 'app_patient_index', methods: ['GET'])]
-    public function index(PatientRepository $patientRepository): Response
+    public function index(): Response
     {
-        return $this->render('patient/index.html.twig', [
-            'patients' => $patientRepository->findAll(),
-        ]);
+        return $this->render('patient/index.html.twig');
     }
 
     #[Route('/new', name: 'app_patient_new', methods: ['GET', 'POST'])]
@@ -33,14 +39,60 @@ class PatientController extends AbstractController
             $entityManager->persist($patient);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_patient_index', [], Response::HTTP_SEE_OTHER);
+            return $this->render(
+                'patient/espace.html.twig',
+                ['id' => $patient->getIdPatient(),]
+            );
         }
-
         return $this->render('patient/new.html.twig', [
             'patient' => $patient,
             'form' => $form,
         ]);
     }
+
+    #[Route('/join', name: 'app_patient_join', methods: ['GET', 'POST'])]
+    public function join(Request $request, PatientRepository $patientRepository, FlashyNotifier $flashy, UrlGeneratorInterface $urlGenerator): Response
+    {
+        $url = $urlGenerator->generate('app_patient_index');
+        $form = $this->createFormBuilder()
+            ->add('email', null, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez saisir votre adresse e-mail.']),
+                    new Email(['message' => 'L\'adresse e-mail "{{ value }}" n\'est pas valide.']),
+                ],
+            ])
+            ->add('submit', SubmitType::class, ['label' => 'Aller', 'attr' => ['class' => 'btn btn-primary']])
+            ->add('cancel', ButtonType::class, [
+                'label' => 'Annuler',
+                'attr' => [
+                    'class' => 'btn btn-outline-primary',
+                    'formnovalidate' => 'formnovalidate',
+                    'onclick' => "window.location.href='$url'",
+                ],
+            ])
+            ->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $patient = $patientRepository->findOneBy(['email_P' => $data['email']]);
+            if ($patient) {
+                $flashy->success('Bienvenue');
+                return $this->redirectToRoute('app_patient_espace', ['id' => $patient->getIdPatient()]);
+            }
+        }
+        return $this->render('patient/login.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+
+    #[Route('/espace/{id}', name: 'app_patient_espace', methods: ['GET'])]
+    public function espace(int $id): Response
+    {
+        return $this->render('patient/espace.html.twig', ['id' => $id]);
+    }
+
 
     #[Route('/{id}', name: 'app_patient_show', methods: ['GET'])]
     public function show(Patient $patient): Response
@@ -51,15 +103,16 @@ class PatientController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_patient_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Patient $patient, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, EntityManagerInterface $entityManager, int $id, PatientRepository $rep): Response
     {
+        $patient = $rep->find($id);
         $form = $this->createForm(PatientType::class, $patient);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($patient);
             $entityManager->flush();
-
-            return $this->redirectToRoute('app_patient_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_patient_show', ['id' => $patient->getIdPatient(),], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('patient/edit.html.twig', [
@@ -75,5 +128,74 @@ class PatientController extends AbstractController
         $entityManager->remove($patient);
         $entityManager->flush();
         return $this->redirectToRoute('app_patient_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+
+
+    #---------------------------------ADMIN--------------------------------
+
+
+
+    #[Route('/admin', name: 'app_patient_list_admin', methods: ['GET'])]
+    public function indexAdmin(PatientRepository $patientRepository): Response
+    {
+        return $this->render('admin_patient/dashboard__tables.html.twig', [
+            'patients' => $patientRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/admin/new', name: 'app_patient_new_admin', methods: ['GET', 'POST'])]
+    public function newPatient(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $patient = new Patient();
+        $form = $this->createForm(PatientType::class, $patient);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($patient);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_patient_list_admin', [], Response::HTTP_SEE_OTHER);
+        }
+        return $this->render('admin_patient/dashboard__new_patient.html.twig', [
+            'patient' => $patient,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/admin/{id}', name: 'app_patient_show_admin', methods: ['GET'])]
+    public function showPatient(Patient $patient): Response
+    {
+        return $this->render('admin_patient/dashboard__show__patient.html.twig', [
+            'patient' => $patient,
+        ]);
+    }
+
+    #[Route('/admin/{id}/edit', name: 'app_patient_edit_admin', methods: ['GET', 'POST'])]
+    public function editAdmin(Request $request, EntityManagerInterface $entityManager, int $id, PatientRepository $rep): Response
+    {
+        $patient = $rep->find($id);
+        $form = $this->createForm(PatientType::class, $patient);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($patient);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_patient_list_admin', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin_patient/dashboard__update__patient.html.twig', [
+            'patient' => $patient,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/admin/{id}', name: 'app_patient_delete_admin', methods: ['POST'])]
+    public function deletePatient(EntityManagerInterface $entityManager, PatientRepository $rep, $id): Response
+    {
+        $patient = $rep->find($id);
+        $entityManager->remove($patient);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_patient_list_admin', [], Response::HTTP_SEE_OTHER);
     }
 }
