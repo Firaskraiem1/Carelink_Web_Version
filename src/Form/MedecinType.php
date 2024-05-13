8ed8896793fd7e4db4fb1a8e816ed44da20afe29
ramroumette <mr.toumirami@outlook.fr>
2024-05-13 22:25:04 +0100

    all change for rami work , just rami work !

--- src/Form/MedecinType.php
+++ src/Form/MedecinType.php
@@ -3,9 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Medecin;
-use App\Entity\specialite;
+use App\Entity\Specialite; // Corrected class name to start with an uppercase letter
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,16 +15,14 @@ class MedecinType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('adresse')
+            ->add('nomDeCabinet')
             ->add('tel')
-            ->add('horairesConsultation')
-            ->add('idSpecialite', EntityType::class, [
-                'class' => specialite::class,
-'choice_label' => 'nom',
-            ])
-        ;
+            ->add('adresse')
+            ->add('email')
+            ->add('heures')
+            ->add('licenceMedicalId')
+            // Add file upload field for image path profile
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
